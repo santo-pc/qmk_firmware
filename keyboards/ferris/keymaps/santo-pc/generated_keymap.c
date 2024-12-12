4d31d3351a397c8c05c5870fa9c96b1f924500a5
santiago <sapalacio2@gmail.com>
2024-12-12 23:39:14 +0100

    o home row as ctrl, swap ctrl and cmd

--- keyboards/ferris/keymaps/santo-pc/generated_keymap.c
+++ keyboards/ferris/keymaps/santo-pc/generated_keymap.c
@@ -4,5 +4,3 @@
         [ "KC_ESC", "KC_NO", "KC_NO", "KC_NO", "KC_NO", "KC_NO", "KC_NO", "KC_NO", "KC_NO", "KC_DEL", "OS_SHFT", "OS_ALT", "OS_CTRL", "OS_CMD", "KC_NO", "KC_LEFT", "KC_DOWN", "KC_UP", "KC_RGHT", "KC_BSPC", "KC_TAB", "KC_NO", "KC_NO", "KC_NO", "KC_NO", "KC_HOME", "KC_END", "KC_NO", "KC_NO", "KC_ENT", "KC_TRNS", "KC_TRNS", "KC_TRNS", "KC_TRNS" ], [ "KC_F1", "KC_F2", "KC_F3", "KC_F4", "KC_F5", "KC_F6", "KC_F7", "KC_F8", "KC_F9", "KC_F10", "KC_1", "KC_2", "KC_3", "KC_4", "KC_5", "KC_6", "KC_7", "KC_8", "KC_9", "KC_0", "KC_F11", "KC_NO", "KC_NO", "KC_NO", "KC_MINS", "KC_EQL", "KC_NO", "KC_NO", "KC_NO", "KC_F12", "KC_TRNS", "KC_TRNS", "KC_TRNS", "KC_TRNS" ]
     ]
 }
-
-1
--- keyboards/ferris/keymaps/santo-pc/keymap.c
+++ keyboards/ferris/keymaps/santo-pc/keymap.c
@@ -28,25 +28,25 @@ enum keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_split_3x5_2(
         KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,                   KC_J,       KC_L,   KC_U,       KC_Y,   KC_SCLN,
-        KC_A,       KC_R,       KC_S,       KC_T,       KC_G,                   KC_M,       KC_N,   KC_E,       KC_I,   KC_O,
+        KC_A,       KC_R,       KC_S,       KC_T,       KC_G,                   KC_M,       KC_N,   KC_E,       KC_I,   RCTL_T(KC_O),
         KC_Z,       KC_X,       KC_C,       KC_D,       KC_V,                   KC_K,       KC_H,   KC_COMM,    KC_DOT, KC_SLSH,
                                             LA_NAV,     KC_SPC,                 KC_LSFT,    LA_SYM
     ),
     [SYM] = LAYOUT_split_3x5_2(
         KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,                KC_CIRC,    KC_AMPR,        KC_ASTR,        KC_UNDS,    KC_SCLN,
-        KC_GRV ,    KC_TILD,    KC_LCBR,    KC_LPRN,    KC_LBRC,                KC_QUOT,    OS_CMD,         OS_CTRL,        OS_ALT,     OS_SHFT,
+        KC_GRV ,    KC_TILD,    KC_LCBR,    KC_LPRN,    KC_LBRC,                KC_QUOT,    OS_CTRL,        OS_CMD,        OS_ALT,     OS_SHFT,
         KC_LT,      KC_GT,      KC_RCBR,    KC_RPRN,    KC_RBRC,                KC_PIPE,    KC_PMNS,        KC_EQL,         KC_PPLS,    KC_BSLS,
                                             _______,    _______,                _______, _______
     ),
     [NAV] = LAYOUT_split_3x5_2(
-        KC_ESC,      KC_NO,       KC_NO,        KC_NO,       KC_NO,             KC_NO,      KC_NO,      KC_NO,     KC_NO,   KC_DEL,
-        OS_SHFT,     OS_ALT,      OS_CTRL,      OS_CMD,      KC_NO,             KC_BSPC,    KC_LEFT,    KC_DOWN,   KC_UP,   KC_RGHT,
+        KC_ESC,      KC_NO,       KC_NO,        KC_NO,       KC_NO,             KC_NO,      KC_D,       KC_U,      KC_NO,   KC_DEL,
+        OS_SHFT,     OS_ALT,      OS_CMD,       OS_CTRL,     KC_NO,             KC_BSPC,    KC_LEFT,    KC_DOWN,   KC_UP,   KC_RGHT,
         KC_TAB,      KC_NO,       KC_NO,        KC_NO,       KC_NO,             KC_ENT,     KC_HOME,    KC_END,    KC_NO,   KC_NO,
                                                 _______,    _______,            _______, _______
     ),
     [NUM] = LAYOUT_split_3x5_2(
         KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                  KC_F6,  KC_F7,      KC_F8,      KC_F9,       KC_F10,
-        KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                   KC_NO,  OS_CMD,     OS_CTRL,    OS_ALT,      OS_SHFT,
+        KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                   KC_NO,  OS_CTRL,    OS_CMD,     OS_ALT,      OS_SHFT,
         KC_6,       KC_7,       KC_8,       KC_9,       KC_0,                   KC_NO,  KC_NO,      KC_NO,      KC_F11,      KC_F12,
                                             _______,    _______,                _______, _______
     )
